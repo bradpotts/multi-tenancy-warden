4b528573ec3a82321f4c222421b658b3cabe2d0f
BradPotts <bradley.j.potts@gmail.com>
2015-10-11 22:18:38 +0000

    Trying to fix circleci failure passing locally.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../dummy/config/environment",  __FILE__)
 require "rspec/rails"
 require "capybara/rspec"
+require "capybara/rails"
 require "factory_girl"
 require "database_cleaner"
 
