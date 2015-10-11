5801e1f39dc94e5224fb9807bf763d2b2690ae21
BradPotts <bradley.j.potts@gmail.com>
2015-10-10 20:28:37 -0700

    Updated configuration files to subengine.

--- config/initializers/warden/strategies/password.rb
+++ config/initializers/warden/strategies/password.rb
@@ -1,16 +1,16 @@
 Warden::Strategies.add(:password) do
-  def subdomain
-    ActionDispatch::Http::URL.extract_subdomains(request.host, 1)
-  end
+	def subdomain
+		ActionDispatch::Http::URL.extract_subdomains(request.host, 1)
+	end
 
-  def valid?
-    subdomain.present? && params["user"]
-  end
+	def valid?
+		subdomain.present? && params["user"]
+	end
 
-  def authenticate!
-    return fail! unless account = Cdgsubengine::Account.find_by(subdomain: subdomain)
-    return fail! unless user = account.users.find_by(email: params["user"]["email"])
-    return fail! unless user.authenticate(params["user"]["password"])
-    success! user
-  end
+	def authenticate!
+		return fail! unless account = Subengine::Account.find_by(subdomain: subdomain)
+		return fail! unless user = account.users.find_by(email: params["user"]["email"])
+		return fail! unless user.authenticate(params["user"]["password"])
+		success! user
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,29 @@
-require "cdgsubengine/constraints/subdomain_required"
+require "subengine/constraints/subdomain_required"
 
-Cdgsubengine::Engine.routes.draw do
-  constraints(Cdgsubengine::Constraints::SubdomainRequired) do
-    scope :module => "account" do
-      root :to => "dashboard#index", :as => :account_root
-      get "/sign_in", :to => "sessions#new", :as => :sign_in
-      post "/sign_in", :to => "sessions#create", :as => :sessions
-      get "/sign_up", :to => "users#new", :as => :user_sign_up
-      post "/sign_up", :to => "users#create", :as => :do_user_sign_up
-      get "/account", :to => "accounts#edit", :as => :edit_account
-      patch "/account", :to => "accounts#update"
-      get "/account/plan/:plan_id",
-        :to => "accounts#plan",
-        :as => :plan_account
-      get "/account/subscribe",
-        :to => "accounts#subscribe",
-        :as => :subscribe_account
-      post "/account/confirm_plan",
-        :to => "accounts#confirm_plan",
-        :as => :confirm_plan_account
-    end
-  end
+Subengine::Engine.routes.draw do
+	constraints(Subengine::Constraints::SubdomainRequired) do
+		scope :module => "account" do
+			root :to => "dashboard#index", :as => :account_root
+			get "/sign_in", :to => "sessions#new", :as => :sign_in
+			post "/sign_in", :to => "sessions#create", :as => :sessions
+			get "/sign_up", :to => "users#new", :as => :user_sign_up
+			post "/sign_up", :to => "users#create", :as => :do_user_sign_up
+			get "/account", :to => "accounts#edit", :as => :edit_account
+			patch "/account", :to => "accounts#update"
+			get "/account/plan/:plan_id",
+				:to => "accounts#plan",
+				:as => :plan_account
+			get "/account/subscribe",
+				:to => "accounts#subscribe",
+				:as => :subscribe_account
+			post "/account/confirm_plan",
+				:to => "accounts#confirm_plan",
+				:as => :confirm_plan_account
+		end
+	end
 
-  root "dashboard#index"
-  get "/sign_up", :to => "accounts#new", :as => :sign_up
-  post "/accounts", :to => "accounts#create", :as => :accounts
-
+	root "dashboard#index"
+	get "/sign_up", :to => "accounts#new", :as => :sign_up
+	post "/accounts", :to => "accounts#create", :as => :accounts
 
 end
